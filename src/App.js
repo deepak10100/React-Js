a4f2e1fbc1f5268b920442ff0bd4c15b8a39c14e
Deepak Malakar <80341169+deepak10100@users.noreply.github.com>
2024-02-06 23:54:08 +0530

    06 day

--- src/App.js
+++ src/App.js
@@ -1,45 +1,12 @@
-import { useEffect, useState } from "react";
+import Home from "./components/Home";
+
 
 
 function App() {
-  const [color, setColor] = useState("green");
-  const [count, setCount] = useState(0);
-  const [car, setCar] = useState({
-    brand: "Ford",
-    model: "Mustang",
-    year: "1964",
-    color: "red"
-  });
-  function updatecar() {
-    setCar((pr)=>{
-      return {...pr, color:"blue"}
-    })
-  }
-  
-  // useEffect(()=>{
-  //   setTimeout(() => {
-  //     setCount((count)=>count+1)
-  //   }, 1000);
-  // },[])
 
-  useEffect(() => {
-    let timer = setTimeout(() => {
-    setCount((count) => count + 1);
-  }, 1000);
-
-  return () => clearTimeout(timer)
-  }, []);
   return (
 <>
-  <h1>deepak {color}</h1>
-  <h1> {car.name}</h1>
-  <h1>My {car.brand}</h1>
-      <p>
-        It is a {car.color} {car.model} from {car.year}.
-      </p>
-      <h1>{count}</h1>
-  <button onClick={()=>{setColor("sky blue")}}>change name</button>
-  <button onClick={updatecar}>blue</button>
+ <Home/>
 </>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './home.css'
+export default function Home() {
+  return (
+    <>
+     <p>
+        <span>home</span>
+        <p style={{background:'black'}} >this is a pragraph</p>
+     </p>
+    </>
+  )
+}
